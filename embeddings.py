a0b57f73ca3733713f2340c52d0c52f736a034e0
wjlee <wjlee.ling@gmail.com>
2023-11-17 10:27:47 +0900

    mis.

--- embeddings.py
+++ embeddings.py
@@ -4,12 +4,11 @@ from typing import List
 from openai import OpenAI
 
 
-client = OpenAI(
-    api_key=os.environ["OPENAI_API_KEY"],
-)
-
-
 def get_embeddings(input: List[str]):
+    client = OpenAI(
+        api_key=os.environ["OPENAI_API_KEY"],
+    )
+
     response = client.embeddings.create(
         model="text-embedding-ada-002",
         input=input,
